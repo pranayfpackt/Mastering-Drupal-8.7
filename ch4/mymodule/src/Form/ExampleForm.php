eb250ba7f99e96b05bc7b9d10ec9d3d2ea65e5ec
Matt Glaman <nmd.matt@gmail.com>
2019-06-13 21:19:52 -0500

    Create ExampleForm.php

---
+++ ch4/mymodule/src/Form/ExampleForm.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Drupal\mymodule\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+class ExampleForm extends FormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'mymodule_example_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $form['message'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Message'),
+    ];
+    $form['submit'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Submit'),
+    ];
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    $message = $form_state->getValue('message');
+    if (strlen($message) < 5) {
+      $form_state->setErrorByName('message', $this->t('Message is too short!'));
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $message = $form_state->getValue('message');
+    $this->messenger()->addStatus($this->t('You submitted: :message', [
+      ':message' => $message
+    ]));
+  }
+}

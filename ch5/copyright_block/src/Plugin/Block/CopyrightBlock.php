86ec43a80118ae3feb29cf735903fdf1e992efff
Matt Glaman <nmd.matt@gmail.com>
2019-07-02 22:52:22 -0500

    Add configuration options to realname widget

--- ch5/copyright_block/src/Plugin/Block/CopyrightBlock.php
+++ ch5/copyright_block/src/Plugin/Block/CopyrightBlock.php
@@ -5,13 +5,14 @@ namespace Drupal\copyright_block\Plugin\Block;
 use Drupal\Core\Block\BlockBase;
 
 /**
-* @Block(
-* id = "copyright_block",
-* admin_label = @Translation("Copyright"),
-* category = @Translation("Custom")
-* )
-*/
+ * @Block(
+ * id = "copyright_block",
+ * admin_label = @Translation("Copyright"),
+ * category = @Translation("Custom")
+ * )
+ */
 class CopyrightBlock extends BlockBase {
+
   /**
    * {@inheritdoc}
    */
@@ -23,31 +24,39 @@ class CopyrightBlock extends BlockBase {
       ]),
     ];
   }
-/**
- * {@inheritdoc}
- */
-public function defaultConfiguration() {
-  return [
-   'company_name' => '',
-  ];
-}
-/**
- * {@inheritdoc}
- */
-public function blockForm($form, \Drupal\Core\Form\FormStateInterface $form_state) {
-  $form['company_name'] = [
-    '#type' => 'textfield',
-    '#title' => t('Company name'),
-    '#default_value' => $this->configuration['company_name'],
-  ];
-  return $form;
-}
 
-/**
- * {@inheritdoc}
- */
-public function blockSubmit($form, \Drupal\Core\Form\FormStateInterface $form_state) {
-  $this->configuration['company_name'] = $form_state->getValue('company_name');
-}
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    return [
+      'company_name' => '',
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockForm(
+    $form,
+    \Drupal\Core\Form\FormStateInterface $form_state
+  ) {
+    $form['company_name'] = [
+      '#type' => 'textfield',
+      '#title' => t('Company name'),
+      '#default_value' => $this->configuration['company_name'],
+    ];
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockSubmit(
+    $form,
+    \Drupal\Core\Form\FormStateInterface $form_state
+  ) {
+    $this->configuration['company_name'] = $form_state->getValue('company_name');
+  }
 
 }
--- ch5/mymodule/src/Plugin/Field/FieldWidget/RealNameDefaultWidget.php
+++ ch5/mymodule/src/Plugin/Field/FieldWidget/RealNameDefaultWidget.php
@@ -29,6 +29,7 @@ class RealNameDefaultWidget extends WidgetBase {
       '#default_value' => '',
       '#size' => 25,
       '#required' => $element['#required'],
+      '#access' => !($this->getSetting('name_format') === 'last')
     ];
     $element['last_name'] = [
       '#type' => 'textfield',
@@ -36,8 +37,53 @@ class RealNameDefaultWidget extends WidgetBase {
       '#default_value' => '',
       '#size' => 25,
       '#required' => $element['#required'],
+      '#access' => !($this->getSetting('name_format') === 'first')
     ];
     return $element;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function defaultSettings() {
+    return [
+      'name_format' => 'both',
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsForm(array $form, FormStateInterface $form_state) {
+    $element['name_format'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Name format'),
+      '#options' => [
+        'both' => $this->t('First and last name'),
+        'first' => $this->t('First name only'),
+        'last' => $this->t('Last name only'),
+      ],
+      '#default_value' => $this->getSetting('name_format'),
+    ];
+    return $element;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsSummary() {
+    $summary = [];
+    $format = $this->getSetting('name_format');
+    if ($format === 'first') {
+      $summary[] = $this->t('First name only');
+    }
+    elseif ($format === 'last') {
+      $summary[] = $this->t('Last name only');
+    }
+    else {
+      $summary[] = $this->t('First and last name');
+    }
+    return $summary;
   }
 
 }
